3014afde814d3ed29a80589627999778835f8858
Nilabh Deka <nylaav@gmail.com>
2023-05-17 15:56:55 +0530

    used spacy and identify parts of speech.v1

--- crawlnoun01.py
+++ crawlnoun01.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import sqlite3
+import spacy
 
 # Load the German word list from a file
 with open("german_word_list.txt", "r", encoding="utf-8") as f:
@@ -18,17 +19,21 @@ table_exists = cursor.fetchone()
 
 # If the 'words' table doesn't exist, create it
 if not table_exists:
-    cursor.execute("CREATE TABLE words (word TEXT, article TEXT, part_of_speech TEXT)")
+    cursor.execute("CREATE TABLE words (word TEXT, article TEXT, pos TEXT)")
+else:
+    # Fetch the last row id from the 'words' table
+    cursor.execute("SELECT MAX(rowid) FROM words")
+    last_row_id = cursor.fetchone()[0]
 
-# Fetch the existing words from the database
-cursor.execute("SELECT word FROM words")
-existing_words = [row[0] for row in cursor.fetchall()]
-found_words.extend(existing_words)
+    # Fetch the existing words from the database
+    cursor.execute("SELECT word FROM words WHERE rowid <= ?", (last_row_id,))
+    existing_words = [row[0] for row in cursor.fetchall()]
+    found_words.extend(existing_words)
 
 # Make a request to the Wiki API to retrieve a list of German pages
 response = requests.get("https://de.wiktionary.org/w/api.php?action=query&generator=random&grnnamespace=0&grnlimit=100&prop=info&inprop=url&format=json")
 
-# Extract the German words and their articles from the API response
+# Extract the German words, their articles, and parts-of-speech from the API response
 for page_id, page in response.json()["query"]["pages"].items():
     # Extract the article content
     article_response = requests.get(page["fullurl"])
@@ -40,46 +45,41 @@ for page_id, page in response.json()["query"]["pages"].items():
     for article in ["das", "die", "der"]:
         if article in article_content:
             german_articles.append(article)
-    # Find all the German words in the article content
+    # Find all the German words in the article content along with their part-of-speech tags
     german_words = []
     found_words_set = set(found_words)
     for word in article_content.split():
         if (
             word in german_word_list
+            and word not in german_articles
+            and word not in ["das", "die", "der"]
             and word not in found_words_set
             and word not in existing_words
         ):
             german_words.append(word)
             found_words.append(word)
             found_words_set.add(word)
-    # Insert the German words and their articles into the database
+    # Use spaCy to determine the part-of-speech tags for the German words
+    nlp = spacy.load("de_core_news_sm")
+    pos_tags = []
     for word in german_words:
+        doc = nlp(word)
+        pos_tags.append(doc[0].pos_)
+    # Insert the German words, their articles, and part-of-speech tags into the database
+    for word, pos in zip(german_words, pos_tags):
         article = None
         for german_article in german_articles:
             if f"{german_article} {word}" in article_content:
                 article = german_article
                 break
-        part_of_speech = None
-        # Extract the part of speech for the word
-        part_of_speech_tags = article_soup.find_all("span", class_="wikicode")
-        for tag in part_of_speech_tags:
-            tag_text = tag.text.strip().lower()
-            if "substantiv" in tag_text or "nomen" in tag_text:
-                part_of_speech = "Noun"
-                break
-            elif "adjektiv" in tag_text:
-                part_of_speech = "Adjective"
-                break
-        # Insert the German words, their articles, and part of speech into the database
-        cursor.execute("INSERT INTO words (word, article, part_of_speech) VALUES (?, ?, ?)", (word, article, part_of_speech))
+        cursor.execute("INSERT INTO words (word, article, pos) VALUES (?, ?, ?)", (word, article, pos))
     conn.commit()
 
 # Close the database connection
 conn.close()
 
 # Print the number of words found and the number of words still not crawled
-total_crawled = len(found_words)
+total_crawled = cursor.lastrowid if cursor.lastrowid else 0
 remaining_words = len(german_word_list) - total_crawled
 print(f"Total words crawled: {total_crawled}")
 print(f"Remaining words to be crawled: {remaining_words}")
-
