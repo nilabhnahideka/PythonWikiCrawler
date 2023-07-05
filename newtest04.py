ece5a314e2f41dd8fed32627386cf9c0766e921f
Nilabh Deka <nylaav@gmail.com>
2023-07-05 19:05:29 +0530

    Latest version

--- newtest04.py
+++ newtest04.py
@@ -2,14 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 import sqlite3
 
-# Connect to the SQLite database
 conn = sqlite3.connect('german_words.db')
 cursor = conn.cursor()
 
-# Create a table to store German words and their articles
 cursor.execute("CREATE TABLE IF NOT EXISTS german_words (word TEXT, article TEXT)")
 
-# Function to fetch German words using the Wiktionary API
 def fetch_german_words():
     url = "https://de.wiktionary.org/w/api.php"
     params = {
@@ -17,39 +14,31 @@ def fetch_german_words():
         "format": "json",
         "list": "allpages",
         "apnamespace": "0",
-        "apprefix": "Aa"  # Starting letter for the words (change as needed)
+        "apfrom": get_resume_word()
     }
 
-    cursor.execute("SELECT COUNT(*) FROM german_words")
-    count = cursor.fetchone()[0]
-    if count > 0:
-        params.pop("apprefix")
+    crawled_count = 0
 
     try:
         print("Crawler running...")
         while True:
-            try:
-                response = requests.get(url, params=params)
-                response.raise_for_status()  # Check for HTTP errors
+            response = requests.get(url, params=params)
+            data = response.json()
+            words = data['query']['allpages']
 
-                data = response.json()
-                words = data['query']['allpages']
+            for word in words:
+                article = extract_first_article(word['title'])
+                store_word(word['title'], article)
+                crawled_count += 1
 
-                for word in words:
-                    article = extract_first_article(word['title'])
-                    store_word(word['title'], article)
+            if 'continue' not in data:
+                break
 
-                if 'continue' not in data:
-                    break
-
-                params['apcontinue'] = data['continue']['apcontinue']
-
-            except (requests.RequestException, ValueError) as e:
-                # Handle exceptions such as network errors or JSON parsing errors
-                print(f"An error occurred: {str(e)}. Retrying...")
-
+            params['apcontinue'] = data['continue']['apcontinue']
+            
     except KeyboardInterrupt:
-        print("Crawler paused.")
+        total_crawled_count = get_total_crawled_count()
+        print(f"Crawler paused. Words crawled: {crawled_count}, Total words crawled: {total_crawled_count}")
 
 # Function to extract the first article associated with a German noun
 def extract_first_article(word):
@@ -58,24 +47,21 @@ def extract_first_article(word):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
 
-    # Find the table containing the articles
     table = soup.find("table", class_="wikitable")
 
     if table:
-        # Find all rows in the table
         rows = table.find_all("tr")
 
         for row in rows:
-            # Check if the row contains a header indicating it's a noun
             header = row.find("th")
             if header and header.text.strip() in ["Nominativ", "Genitiv"]:
-                # Find the first article in the row and extract only the article text
-                article = row.find("td").text.strip().split()[0]
-                return article
+                td_element = row.find("td")
+                if td_element:
+                    article = td_element.text.strip().split()[0]
+                    return article
 
     return None
 
-# Function to store a German word and its article in the database
 def store_word(word, article):
     if article is not None:
         cursor.execute("INSERT INTO german_words VALUES (?, ?)", (word, article))
@@ -84,7 +70,16 @@ def store_word(word, article):
 
     conn.commit()
 
-# Check if the database already exists
+def get_resume_word():
+    cursor.execute("SELECT word FROM german_words ORDER BY rowid DESC LIMIT 1")
+    last_word = cursor.fetchone()
+    return last_word[0] if last_word else None
+
+def get_total_crawled_count():
+    cursor.execute("SELECT COUNT(*) FROM german_words")
+    count = cursor.fetchone()[0]
+    return count
+
 cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='german_words'")
 table_exists = cursor.fetchone()
 
@@ -95,5 +90,4 @@ else:
     print("Starting crawler...")
     fetch_german_words()
 
-# Close the database connection
 conn.close()
